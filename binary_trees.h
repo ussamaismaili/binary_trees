8c9fe346b1df046fab1355f6b342719090e65c22
Oussama ismaili <ismailiossama@gmail.com>
2023-11-04 02:42:50 +0100

    edit 2

--- binary_trees.h
+++ binary_trees.h
@@ -2,17 +2,16 @@
 #define BINARY_TREES_H
 
 #include <stddef.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <stdio.h>
 
 /**
- * struct binary_tree_s - Binary tree node
+ * struct binary_tree_s - Binary tree nodee
  * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
+ * @parent: Pointer parent node
+ * @left: Pointer left child node
+ * @right: Pointer right child node
  */
 struct binary_tree_s
 {
@@ -22,9 +21,9 @@ struct binary_tree_s
 	struct binary_tree_s *right;
 };
 typedef struct binary_tree_s binary_tree_t;
-typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
+typedef struct binary_tree_s bst_t;
 
 /**
  * struct levelorder_queue_s - Level order traversal queue.
@@ -37,10 +36,10 @@ typedef struct levelorder_queue_s
 	struct levelorder_queue_s *next;
 } levelorder_queue_t;
 
-/* Printing helper function */
+/* the helper func */
 void binary_tree_print(const binary_tree_t *);
 
-/* Task function prototypes */
+/* Tasks func prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -82,4 +81,4 @@ heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
 
-#endif /* BINARY_TREES_H */
+#endif
